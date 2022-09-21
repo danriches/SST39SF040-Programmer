7108a9d7b78038c1f92da99b28802336e8c6f9af
D R <dan@maybefishing.net>
2022-09-21 11:40:22 +0100

    Added VS2017 Windows Application - Flash Programmer
    This is still not finished but does work to a point. It does miss off the last parts of the GetID function information returned from the Arduino though. Writing, reading and erasing do work but I would use with caution and test everything before trying out on a Flash chip you can't do without!!
    Later features will be to correct the GetID function, streaming the read data and allowing a sector erase as well as the entire chip erase function.
    I'll leave in the old commands so terminal use will still work.
    Enjoy!

---
+++ FlashProgrammer/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCOMPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.btnGetID = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStartAddress = new System.Windows.Forms.TextBox();
+            this.txtEndAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCOMPort
+            // 
+            this.txtCOMPort.Location = new System.Drawing.Point(17, 38);
+            this.txtCOMPort.Name = "txtCOMPort";
+            this.txtCOMPort.Size = new System.Drawing.Size(88, 20);
+            this.txtCOMPort.TabIndex = 0;
+            this.txtCOMPort.Text = "COM5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(111, 38);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(74, 20);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.AcceptsReturn = true;
+            this.txtData.Location = new System.Drawing.Point(17, 94);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtData.Size = new System.Drawing.Size(682, 327);
+            this.txtData.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(338, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hex Data - Changes here are NOT written to flash, edit the original file!";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Enabled = false;
+            this.btnRead.Location = new System.Drawing.Point(483, 60);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(68, 28);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Enabled = false;
+            this.btnWrite.Location = new System.Drawing.Point(557, 60);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(68, 28);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnErase
+            // 
+            this.btnErase.Enabled = false;
+            this.btnErase.Location = new System.Drawing.Point(631, 61);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(68, 27);
+            this.btnErase.TabIndex = 7;
+            this.btnErase.Text = "Erase";
+            this.btnErase.UseVisualStyleBackColor = true;
+            this.btnErase.Click += new System.EventHandler(this.btnErase_Click);
+            // 
+            // btnGetID
+            // 
+            this.btnGetID.Enabled = false;
+            this.btnGetID.Location = new System.Drawing.Point(411, 61);
+            this.btnGetID.Name = "btnGetID";
+            this.btnGetID.Size = new System.Drawing.Size(66, 27);
+            this.btnGetID.TabIndex = 8;
+            this.btnGetID.Text = "Get ID";
+            this.btnGetID.UseVisualStyleBackColor = true;
+            this.btnGetID.Click += new System.EventHandler(this.btnGetID_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(191, 37);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(74, 20);
+            this.btnDisconnect.TabIndex = 9;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(271, 41);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(75, 13);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Status: Closed";
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Location = new System.Drawing.Point(17, 452);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessages.Size = new System.Drawing.Size(681, 85);
+            this.txtMessages.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Message Log";
+            // 
+            // txtStartAddress
+            // 
+            this.txtStartAddress.Location = new System.Drawing.Point(411, 31);
+            this.txtStartAddress.Name = "txtStartAddress";
+            this.txtStartAddress.Size = new System.Drawing.Size(139, 20);
+            this.txtStartAddress.TabIndex = 13;
+            this.txtStartAddress.Text = "0";
+            // 
+            // txtEndAddress
+            // 
+            this.txtEndAddress.Location = new System.Drawing.Point(557, 31);
+            this.txtEndAddress.Name = "txtEndAddress";
+            this.txtEndAddress.Size = new System.Drawing.Size(139, 20);
+            this.txtEndAddress.TabIndex = 14;
+            this.txtEndAddress.Text = "524287";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(407, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Start Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(554, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "End Address";
+            // 
+            // btnClearData
+            // 
+            this.btnClearData.Location = new System.Drawing.Point(524, 427);
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.Size = new System.Drawing.Size(77, 22);
+            this.btnClearData.TabIndex = 17;
+            this.btnClearData.Text = "Clear Data";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.btnClearData_Click);
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(622, 427);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(77, 22);
+            this.btnClearLog.TabIndex = 18;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 29);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 546);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClearLog);
+            this.Controls.Add(this.btnClearData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEndAddress);
+            this.Controls.Add(this.txtStartAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMessages);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnGetID);
+            this.Controls.Add(this.btnErase);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCOMPort);
+            this.Name = "Form1";
+            this.Text = "SST39SF0x0A Flash Programmer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCOMPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.Button btnGetID;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStartAddress;
+        private System.Windows.Forms.TextBox txtEndAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FlashProgrammer/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //File Open Dialog for writing to Flash, we'll need to add a File Save one as well for Dumped data once we've also done the read processing
+        private System.Windows.Forms.OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+
+        SerialPort serial = new SerialPort();
+        bool _continue;
+
+        //Used to store the file we read in before writing to Flash
+        byte[] writeData;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Title = "Select file to flash to Flash, Flashy eh!?";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Find last com port and list it in the control.
+
+        }
+
+        private void AddToMessageLog(string textToAdd)
+        {
+            txtMessages.Text += textToAdd;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //Set default parameters for the serial port and open it
+            serial.Parity = Parity.None;
+            serial.ReadBufferSize = 256;
+            serial.StopBits = StopBits.One;
+            serial.WriteBufferSize = 256;
+            serial.DataBits = 8;
+            serial.BaudRate = 115200;
+            serial.Handshake = Handshake.None;
+            //serial.ReadTimeout = 1500;
+            //serial.WriteTimeout = 1500;
+            serial.PortName = txtCOMPort.Text;
+            serial.Open();
+            _continue = true;
+
+            //Add handler to handle incoming data packets
+            serial.DataReceived += new SerialDataReceivedEventHandler(Serial_DataReceived);
+
+            if (serial.IsOpen)
+            {
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                lblStatus.Text = "Status: Connected";
+
+                btnGetID.Enabled = true;
+                btnRead.Enabled = true;
+                btnWrite.Enabled = true;
+                btnErase.Enabled = true;
+            } else
+            {
+                lblStatus.Text = "Status: Unable to connect!";
+                btnGetID.Enabled = false;
+                btnRead.Enabled = false;
+                btnWrite.Enabled = false;
+                btnErase.Enabled = false;
+            }
+        }
+
+        private delegate void SetTextDeleg(string text);            //Hex Data box text
+        private delegate void SetMsgTextDeleg(string text);         //Message box text
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //try
+            //{ 
+            string data = serial.ReadLine();        //This line is problematic and maybe should be serial.ReadBytes???
+            if (data.StartsWith("0x"))
+                this.BeginInvoke(new SetTextDeleg(SerialIn_DataReceived), new object[] { data });
+            else
+                this.BeginInvoke(new SetMsgTextDeleg(SerialIn_DataReceived2), new object[] { data });
+            //} catch(Exception ex)
+            //{
+            //}
+        }
+
+        private void SerialIn_DataReceived(string data) { txtData.Text += data + Environment.NewLine; }
+        private void SerialIn_DataReceived2(string data) { txtMessages.Text += data + Environment.NewLine; }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serial.IsOpen)
+            {
+                serial.Close();
+                serial.DataReceived -= new SerialDataReceivedEventHandler(Serial_DataReceived);
+            }
+
+            if (!serial.IsOpen)
+            {
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnGetID.Enabled = false;
+                btnRead.Enabled = false;
+                btnWrite.Enabled = false;
+                btnErase.Enabled = false;
+                lblStatus.Text = "Status: Closed";
+            } else
+            {
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnGetID.Enabled = true;
+                btnRead.Enabled = true;
+                btnWrite.Enabled = true;
+                btnErase.Enabled = true;
+                lblStatus.Text = "Status: Open";
+            }
+        }
+
+        private void btnGetID_Click(object sender, EventArgs e)
+        {
+            if(serial.IsOpen)
+            {
+                serial.WriteLine("<I,0,0>");
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //This needs the same treatment as the write function, namely streaming data back so it's faster
+            if(serial.IsOpen)
+            {
+                serial.WriteLine("<D," + txtStartAddress.Text + "," + txtEndAddress.Text + ">");
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            //Warn that the chip must be erased before hand, do they want to erase it first or attempt an overwrite which may go wrong!?!?!
+            MessageBox.Show("The chip should be erased before writing or you'll end up with dodgy data stored in it!", "Warning!");
+
+            //Open a file dialog and allow any file up to a max of 524288 bytes in length.
+            openFileDialog1.ShowDialog();
+
+            string filename = openFileDialog1.FileName;
+            if (filename == "")
+            {
+                txtMessages.Text += "Flash was not written to, user cancelled operation!";
+                return;
+            }
+            else
+            {
+                txtMessages.Text += "File selected was: " + filename;
+                string result = WriteFileToFlash(filename);
+                txtMessages.Text += result;
+            }
+            //Construct a base command with address counter and write the data byte by byte as is required by the chip.
+        }
+
+        string WriteFileToFlash(string filename)
+        {
+            string result = "FAILED!";
+
+            //Open the file and read into a byte array
+            byte[] dataArray = File.ReadAllBytes(filename);
+
+            //Write the bytes one at a time until done
+            if (!serial.IsOpen)
+                return "Failed to Write as not connected to Arduino Mega!";
+
+            txtMessages.Text += "Writing Flash starting at address 0x0000. Please Wait...";
+            int addressCounter = 0;
+
+            //send <X,0,0> then <Z,x,0>   where x is 256 or the number of bytes to write, lastly send <Y> and then the full amount of bytes in sequence  
+            //- Arduino side seems to work just need this side to
+            int totalSize = dataArray.Length;
+
+            serial.WriteLine("<X," + txtStartAddress.Text + ",0>");
+            serial.WriteLine("<Z," + totalSize.ToString() + ",0>");
+            serial.Write("<Y>");
+
+            int offset = 0;
+            int block = 256;
+            if (block > totalSize)
+                block = totalSize;
+            
+            //This part needs checking before allowing a write to Flash!!!!!!!
+            while(offset < totalSize)
+            {                
+                serial.Write(dataArray, offset, block);
+                if (offset + block > totalSize)
+                    offset = totalSize - (offset + block);
+                else
+                    offset += block;
+                Application.DoEvents();
+            }
+            //Read the bytes back and compare, using a dump command, ignore and don't read data past where it was written...
+
+            return "Success: Wrote " + (addressCounter + 1).ToString() + " bytes to Flash." + Environment.NewLine;
+        }
+
+        private void btnErase_Click(object sender, EventArgs e)
+        {
+            if(serial.IsOpen)
+            {
+                txtMessages.Text += "Erasing entire chip, I hope you wanted to do this as it's now too late!";
+                serial.WriteLine("<E,0,0>");
+            }
+        }
+
+        private void btnClearData_Click(object sender, EventArgs e)
+        {
+            txtData.Text = "";
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            txtMessages.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serial.WriteLine("<Z,10,0>");
+            serial.Write("<Y>");
+            byte[] x = new byte[10] { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
+            serial.Write(x, 0, 10);
+        }
+    }
+}
